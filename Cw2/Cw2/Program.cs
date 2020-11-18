f2367ae8c505d9f0978e91069a6cfda0a334b5aa
s16537 <s16537@pjwstk.edu.pl>
2020-11-18 18:51:14 +0100

    parameters handling

--- Cw2/Cw2/Program.cs
+++ Cw2/Cw2/Program.cs
@@ -6,7 +6,45 @@ namespace Cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //program parameters initialized with default values 
+            string inputFile = "data.csv";
+            string destPath = "result.xml";
+            string format = "xml";
+            foreach(string param in args)
+            {
+                bool inputInfo = param.IndexOf(".csv", StringComparison.OrdinalIgnoreCase) >= 0;
+                bool outputXML = param.IndexOf(".xml", StringComparison.OrdinalIgnoreCase) >= 0;
+                bool outputJSON = param.IndexOf(".json", StringComparison.OrdinalIgnoreCase) >= 0;
+                bool formatXML = string.Equals(param, "xml", StringComparison.OrdinalIgnoreCase);
+                bool formatJSON = string.Equals(param, "json", StringComparison.OrdinalIgnoreCase);
+
+                if (inputInfo)
+                {
+                    inputFile = param;
+                }
+                else if (outputXML)
+                {
+                    destPath = param;
+                }
+                else if (outputJSON)
+                {
+                    destPath = param;
+                }
+                else if (formatXML)
+                {
+                    format = param;
+                }
+                else if (formatJSON)
+                {
+                    format = param;
+                }
+            }
+
+            Console.WriteLine(inputFile);
+            Console.WriteLine(destPath);
+            Console.WriteLine(format);
+        
+
         }
     }
 }
